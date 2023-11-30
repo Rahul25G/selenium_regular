2b54a24c4081cd590859607a0258ed9e1ead2e32
Kiran <Kiran@KiranGundPC>
2023-11-30 13:22:26 +0530

    updates

---
+++ src/selenium_regular/Object.java
@@ -0,0 +1,129 @@
+package selenium_regular;
+
+public class Object {
+	public static void main(String[] arg) {
+		
+		class Human
+		{		
+			String name;
+			int age;
+			double height;
+			double weight;
+			String state;
+			
+			void speak(String content) {
+			}
+		}
+	
+	class Table
+	{
+		String color;
+		String shape;
+		double height;
+		double width;
+		double length;
+		
+		void put() {
+			
+		}
+	}
+	
+	class Laptop {
+		String company;
+		String model;
+		int ram;
+		int nooflaptops ;
+		
+		void workdone() {
+			
+		}
+
+	}
+	
+	class Cricket {
+		String playername;
+		int noofplayer;
+		String teamname;
+		String position;
+		
+		void play() {
+			
+		}
+
+	}
+	
+	class Car {
+		String color;
+		int noofwheels;
+		String company;
+		
+		void drive()
+		{
+			
+		}
+}
+	class Book {
+		String name;
+		String author;
+		int pages;
+		
+		void read()
+		{
+			
+		}
+}
+	class Movie {
+		String title;
+		String director;
+		String cast;
+		int year;
+		int tickets;
+		
+		void watch(){}
+}
+	class Mobile {
+		String brand;
+		String model;
+		String os;
+		int storage;
+		
+		void call()
+		{
+			
+		}
+	}
+	class Teacher {
+		String name;
+		String subject;
+		int age;
+		
+		void Study()
+		{
+			
+		}
+	}
+	class notebook
+	{
+		String shape;
+		double height;
+		int pages;				
+		void towrite(String content) {
+			}
+	}
+	
+
+
+	
+
+	
+
+	
+	
+
+	
+	
+	
+	
+	
+
+}
